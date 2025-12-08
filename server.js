6c93a5873f87e79ecfa41db19ae86cf0a3f44d7e
Felipe Tramontin <felipetramontin2008@gmail.com>
2025-12-07 22:55:42 -0500

    Changed to port 8080

--- server.js
+++ server.js
@@ -7,5 +7,8 @@ app.use(express.static('public')); // serve frontend
 
 app.use('/api/exercises', exercisesRouter);
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
