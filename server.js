95830fbab1ea1bdacb153a7e0d122d719fd95d28
Felipe Tramontin <felipetramontin2008@gmail.com>
2025-12-07 23:18:44 -0500

    8080

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static('public')); // serve frontend
 
 app.use('/api/exercises', exercisesRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
